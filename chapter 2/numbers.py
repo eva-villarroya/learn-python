df91f4178d5f82de63d1632346ac28c635a8fd92
personal <personal@evas-air.t.hu>
2019-08-30 14:39:12 +0200

    Chapter 2 exercises

---
+++ chapter 2/numbers.py
@@ -0,0 +1,13 @@
+# this is a comment
+
+print("Sum 2 + 2 = " + str(2 + 2))
+print("Sub 2 - 2 = " + str(2 - 2))
+print("Mul 2 * 2 = " + str(2 * 2))
+print("Div 2 / 2 = " + str(2 / 2))
+print("Exp 2 ^ 2 = " + str(2 ** 2))
+
+print("Precedence of ops 2 + 3 * 4 = " + str(2 + 3 * 4))
+print("Precedence of ops (2 + 3) * 4 = " + str((2 + 3) * 4))
+
+print("Decimals 0.1 + 0.1 = " + str(0.1 + 0.1))
+print("Decimals 0.2 + 0.1 = " + str(0.2 + 0.1))
---
+++ chapter 2/strings.py
@@ -0,0 +1,18 @@
+message = "this is a tEst"
+
+print(message)
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+for_you = " for you"
+
+print(message + for_you)
+print(message.upper() + for_you.title())
+
+print("Text with \t TAB")
+print("Text\nwith\nnew\nline")
+
+print("'" + " Other functions ".rstrip() + "'")
+print("'" + " Other functions ".lstrip() + "'")
+print("'" + " Other functions ".strip() + "'")
